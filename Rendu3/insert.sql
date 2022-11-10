6b25e27b4416b6e5782e72f96ce9e1e7ce3c11a3
Thomas <tforest@student.agh.edu.pl>
2022-11-10 13:09:44 +0100

    insert

--- Rendu3/insert.sql
+++ Rendu3/insert.sql
@@ -1,7 +1,6 @@
-
-INSERT INTO Ressource VALUES (1, 'star wars',TO_DATE('19990516','YYYYMMDD'), 'presses de la cité', 'science-fiction',  147 );
-INSERT INTO Ressource VALUES (2, 'harry potter',TO_DATE('19970626','YYYYMMDD'), 'gallimard jeunesse', 'fantaisie',  187 );
-INSERT INTO Ressource VALUES (4, 'symphonie de beethoven',TO_DATE('20000626','YYYYMMDD'), 'musiqueclassique', 'classique',  777 );
+INSERT INTO Ressource VALUES (1, 'star wars',TO_DATE('1999-05-16', 'YYYY-MM-DD'), 'presses de la cité', 'science-fiction',  147 );
+INSERT INTO Ressource VALUES (2, 'harry potter',TO_DATE('1997-06-26', 'YYYY-MM-DD'), 'gallimard jeunesse', 'fantaisie',  187 );
+INSERT INTO Ressource VALUES (4, 'symphonie de beethoven',TO_DATE('2000-06-26', 'YYYY-MM-DD'), 'musiqueclassique', 'classique',  777 );
 
 
 INSERT INTO Livre VALUES (2, 48756, 'histoire, se situant dans les années 1990, raconte la jeunesse de Harry Potter, sorcier orphelin élevé sans affection ni considération par la seule famille vivante qui lui reste : son oncle et sa tante moldus (sans pouvoirs magiques). Le garçon découvre son identité de sorcier, son héritage tragique et la responsabilité qui lui revient', 'anglais');
@@ -11,13 +10,12 @@ INSERT INTO Musique VALUES (4,'15:02:00');
 INSERT INTO Film VALUES (1,'L"histoire de Star Wars se déroule dans une galaxie qui est le théâtre d"affrontements entre les Chevaliers Jedi et les Seigneurs noirs des Sith, personnes sensibles à la Force, un champ énergétique mystérieux leur procurant des pouvoirs psychiques. Les Jedi maîtrisent le Côté lumineux de la Force, pouvoir bénéfique et défensif, pour maintenir la paix dans la galaxie. Les Sith utilisent le Côté obscur, pouvoir nuisible et destructeur, pour leurs usages personnels et pour dominer la galaxie','01:36:00', 'français');
 
 
-INSERT INTO Contributeur VALUES (1, 'verne', 'jules', TO_DATE('18280208','YYYYMMDD'), 'français');
-INSERT INTO Contributeur VALUES (2, 'mozart', 'johannes', TO_DATE('17560127','YYYYMMDD'), 'autrichien');
+INSERT INTO Contributeur VALUES (1, 'verne', 'jules', TO_DATE('1828-08-08', 'YYYY-MM-DD'), 'français');
+INSERT INTO Contributeur VALUES (2, 'mozart', 'johannes', TO_DATE('1828-08-08', 'YYYY-MM-DD'), 'autrichien');
 
 
 
 INSERT INTO Compositeur VALUES (2,7);
-I
 
 INSERT INTO Interprete VALUES (2,7);
 
@@ -44,4 +42,4 @@ INSERT INTO Adherent VALUES ('richard.duchateau@laposte.fr', 'duchateau', 'richa
 INSERT INTO Exemplaire VALUES (1,'3.44','neuf','1',1);
 
 
-INSERT INTO Pret VALUES (1,TO_DATE('20200914','YYYYMMDD'),'paul.russel@gmail.com','russel','paul',25,'retard',21,NULL,NULL,'0');
+INSERT INTO Pret VALUES (1,TO_DATE('1828-08-08', 'YYYY-MM-DD'),'paul.russel@gmail.com','russel','paul',25,'retard',21,NULL,NULL,'0');

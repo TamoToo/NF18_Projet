44d1951cb0100289daa16f69e000a9c64bfc2f6f
Cyril Normand <cyril.normand@etu.utc.fr>
2022-11-14 21:48:00 +0000

    Update insert.sql

--- Rendu3/insert.sql
+++ Rendu3/insert.sql
@@ -1,18 +1,18 @@
-INSERT INTO Ressource VALUES (1, 'star wars', TO_DATE('1999-05-16', 'YYYY-MM-DD'), 'presses de la cité', 'science-fiction',  147 );
-INSERT INTO Ressource VALUES (2, 'harry potter', TO_DATE('1997-06-26', 'YYYY-MM-DD'), 'gallimard jeunesse', 'fantaisie',  187 );
-INSERT INTO Ressource VALUES (4, 'symphonie de beethoven', TO_DATE('2000-06-26', 'YYYY-MM-DD'), 'musiqueclassique', 'classique',  777 );
+INSERT INTO Ressource VALUES (1, 'top gun', TO_DATE('1986-09-17', 'YYYY-MM-DD'), 'paramount pictures', 'action',  115 );
+INSERT INTO Ressource VALUES (2, 'harry potter', TO_DATE('1997-06-26', 'YYYY-MM-DD'), 'gallimard jeunesse', 'fantaisie',  299 );
+INSERT INTO Ressource VALUES (4, 'tchikita', TO_DATE('2016-09-19', 'YYYY-MM-DD'), 'jul', 'rap',  334 );
 
 
-INSERT INTO Livre VALUES (2, 48756, 'histoire, se situant dans les années 1990, raconte la jeunesse de Harry Potter, sorcier orphelin élevé sans affection ni considération par la seule famille vivante qui lui reste : son oncle et sa tante moldus (sans pouvoirs magiques). Le garçon découvre son identité de sorcier, son héritage tragique et la responsabilité qui lui revient', 'anglais');
+INSERT INTO Livre VALUES (2, 34573, 'Harry Potter est un garçon ordinaire. Mais, le jour de ses onze ans, son existence bascule : un géant vient le chercher pour l emmener dans une école de sorciers. Voler à cheval sur des balais, jeter des sorts, combattre les Trolls: Harry Potter se révèle être un sorcier vraiment doué.', 'anglais');
 
 INSERT INTO Musique VALUES (4,'15:02:00');
 
-INSERT INTO Film VALUES (1,'L"histoire de Star Wars se déroule dans une galaxie qui est le théâtre d"affrontements entre les Chevaliers Jedi et les Seigneurs noirs des Sith, personnes sensibles à la Force, un champ énergétique mystérieux leur procurant des pouvoirs psychiques. Les Jedi maîtrisent le Côté lumineux de la Force, pouvoir bénéfique et défensif, pour maintenir la paix dans la galaxie. Les Sith utilisent le Côté obscur, pouvoir nuisible et destructeur, pour leurs usages personnels et pour dominer la galaxie','01:36:00', 'français');
+INSERT INTO Film VALUES (1,'Dans l océan Indien à la fin des années 1980, durant les dernières années de la guerre froide. Au cours d une patrouille aérienne de routine, deux chasseurs F-14 Tomcat de l US Navy embarqués sur le porte-avions USS Enterprise rencontrent deux chasseurs MiG-28 soviétiques, au comportement hostile. Les pilotes américains parviennent à mettre les avions soviétiques en fuite mais le leader de la formation, Bill « Cougar » Cortell, se retrouve en état de choc à la suite de l accrochage. Le pilote de l autre appareil américain, Pete « Maverick » Mitchell — un aviateur avec un tempérament de « chien fou », peu apprécié de sa hiérarchie et marqué par la disparition en vol de son père, le 5 novembre 1965, lors de la guerre du Viêt Nam — retourne alors chercher Cougar, malgré les ordres reçus et le manque de kérosène de son avion, et l aide à se poser sur le porte-avions.','01:40:00', 'français');
 
 
-INSERT INTO Contributeur VALUES (1, 'verne', 'jules', TO_DATE('1828-08-08', 'YYYY-MM-DD'), 'français');
-INSERT INTO Contributeur VALUES (4, 'mozart', 'johannes', TO_DATE('1828-08-08', 'YYYY-MM-DD'), 'autrichien');
-INSERT INTO Contributeur VALUES (2, 'dicaprio', 'leonardo', TO_DATE('1974-11-11', 'YYYY-MM-DD'), 'américain');
+INSERT INTO Contributeur VALUES (1, 'cruise', 'tom', TO_DATE('1862-07-03', 'YYYY-MM-DD'), 'americain');
+INSERT INTO Contributeur VALUES (4, 'mari', 'julien', TO_DATE('1990-01-14', 'YYYY-MM-DD'), 'francais');
+INSERT INTO Contributeur VALUES (2, 'radcliffe', 'daniel jacob', TO_DATE('1989-07-23', 'YYYY-MM-DD'), 'britannique');
 
 INSERT INTO Exemplaire VALUES (1, TRUE, 'neuf');
 INSERT INTO Exemplaire VALUES (2, TRUE, 'bon');
@@ -34,16 +34,16 @@ INSERT INTO Realisateur VALUES (1,1);
 
 
 
-INSERT INTO Personne VALUES ('jules.verne@etu.utc.fr', 'verne', 'jules', 'rue de la paix');
-INSERT INTO Personne VALUES ('jacques.chirac@etu.utc.fr', 'chirac', 'jacques', 'avenue de la république');
+INSERT INTO Personne VALUES ('tom.cruise@etu.utc.fr', 'cruise', 'tom', 'rue de l avion');
+INSERT INTO Personne VALUES ('tom.holland@etu.utc.fr', 'holland', 'tom', 'avenue de l araignée');
 
-INSERT INTO Adherent VALUES ('jules.verne@etu.utc.fr', TO_DATE('1828-02-08', 'YYYY-MM-DD'), 0667897621, 'ze154d' );
+INSERT INTO Adherent VALUES ('tom.cruise@etu.utc.fr', TO_DATE('1862-07-03', 'YYYY-MM-DD'), 0669354585, 'zebi13' );
 
-INSERT INTO Personnel VALUES ('jacques.chirac@etu.utc.fr', 'directeur');
+INSERT INTO Personnel VALUES ('tom.holland@etu.utc.fr', 'directeur');
 
-INSERT INTO Compte_Utilisateur VALUES ('verneju','jules.verne@etu.utc.fr', 'azerty123');
+INSERT INTO Compte_Utilisateur VALUES ('cruisetom','tom.cruise@etu.utc.fr', 'tomi60200');
 
 
-INSERT INTO Pret VALUES (2, 'jules.verne@etu.utc.fr', TO_DATE('2015-02-08', 'YYYY-MM-DD'), 7);
-INSERT INTO Retard VALUES (1, 4, 'jules.verne@etu.utc.fr');
-INSERT INTO Deterioration VALUES (1, 'abimé', 0, 'jules.verne@etu.utc.fr');
+INSERT INTO Pret VALUES (2, 'tom.cruise@etu.utc.fr', TO_DATE('2013-02-07', 'YYYY-MM-DD'), 3);
+INSERT INTO Retard VALUES (1, 4, 'tom.cruise@etu.utc.fr');
+INSERT INTO Deterioration VALUES (1, 'abimé', 0, 'tom.cruise@etu.utc.fr');

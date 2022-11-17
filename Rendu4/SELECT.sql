45e6c1c4f871ab9c5fc79b1126a6fc62e01220e4
Thomas <tforest@student.agh.edu.pl>
2022-11-17 13:10:13 +0100

    update select

--- Rendu4/SELECT.sql
+++ Rendu4/SELECT.sql
@@ -4,34 +4,34 @@
 
 
 --"Afficher les ressources disponibles"
-SELECT titre FROM Ressources 
-JOIN Exemplaire ON Exemplaire.code = Ressource.code 
-WHERE disponibilité = TRUE;
+SELECT titre FROM Ressource 
+JOIN Exemplaire ON Exemplaire.code_ressource = Ressource.code 
+WHERE disponibilite = TRUE;
 
---"Renvoie le nombre de livre empruntés par Tom Holland" 
-SELECT COUNT(*) FROM Ressources 
-JOIN Ressources ON Ressources.code = Pret.code 
-JOIN Adhérent ON Adhérent.email = Pret.adhérent
-JOIN Personne ON Personne.email = Adhérent.email 
-WHERE nom = Holland AND prenom = Tom
+--"Renvoie le nombre de livre empruntes par Tom Holland" 
+SELECT COUNT(*) FROM Ressource 
+JOIN Pret ON Ressource.code = Pret.code 
+JOIN Adherent ON Adherent.email = Pret.adherent
+JOIN Personne ON Personne.email = Adherent.email 
+WHERE Personne.nom = "holland" AND Personne.prenom = "tom";
 
---"Renvoie le nombre de personne ayant emprunté le film top gun"
-SELECT COUNT(*) FROM Adhérent 
-JOIN Pret ON Pret.adhérent = Adhérent.email
-JOIN Ressources ON Ressources.code = Pret.code
-WHERE Ressources.titre = "top gun" 
+--"Renvoie le nombre de personne ayant emprunte le film top gun"
+SELECT COUNT(*) FROM Adherent 
+JOIN Pret ON Pret.adherent = Adherent.email
+JOIN Ressource ON Ressource.code = Pret.code
+WHERE Ressource.titre = "top gun"; 
 
 
---"Renvoie la liste des personnes ayant perud l'article empreinté"
+--"Renvoie la liste des personnes ayant perud l'article empreinte"
 SELECT nom , prenom FROM Personne 
-JOIN Adhérent ON Adhérent.email = Personne.email
-JOIN Deterioration ON deterioration.adhérent = Adhérent.email 
-WHERE Deterioration.nv_etat = perdu
+JOIN Adherent ON Adherent.email = Personne.email
+JOIN Deterioration ON deterioration.adherent = Adherent.email 
+WHERE Deterioration.nv_etat = "perdu";
 
---"Renvoie la liste des personnes ayant eu des jours de retards, triés par le nombre de jours de retard le plus élevé au moins élevé"
+--"Renvoie la liste des personnes ayant eu des jours de retards, tries par le nombre de jours de retard le plus eleve au moins eleve"
 SELECT nom , prenom FROM Personne 
-JOIN Adhérent ON Adhérent.email = Personne.email 
-JOIN Retard ON Retard.email = Adhérent.email
+JOIN Adherent ON Adherent.email = Personne.email 
+JOIN Retard ON Retard.adherent = Adherent.email
 WHERE nb_jours > 0
 ORDER BY nb_jours DESC;
 
@@ -39,25 +39,25 @@ ORDER BY nb_jours DESC;
 SELECT max(nb_jours) FROM Retard;
 
 --"Renvoie la proportion des personnes suspendu"
-SELECT  COUNT(suspendu)/COUNT(*) FROM Adhérent WHERE suspendu = TRUE;
+SELECT  COUNT(suspension)/COUNT(*) FROM Adherent WHERE suspension = TRUE;
 
 --"Revnoie la moyenne des jours de retards"
 SELECT AVG(nb_jours) FROM Retard;
  
---"Classement des ressources empruntées de la plus empruntée à la moins empruntée "
+--"Classement des ressources empruntees de la plus empruntee à la moins empruntee "
 
 SELECT COUNT(*) AS Nombre_d_emprunt,R.titre
 FROM Ressource R
-INNER JOIN Exemplaire E ON E.ressource = R.code
-INNER JOIN Pret P ON P.code = E.code
+INNER JOIN Exemplaire E ON E.code_ressource = R.code
+INNER JOIN Pret P ON P.code = E.code_ressource
 GROUP BY R.code 
 ORDER BY COUNT(*) DESC;
 
---"En combien d'exemplaires disponibles existe une ressource donnée (ici la ressource 1) "
+--"En combien d'exemplaires disponibles existe une ressource donnee (ici la ressource 1) "
 
 SELECT COUNT(*) AS nb_exemplaires_dispo_ressource1
 FROM Exemplaire
-WHERE Exemplaire.ressource = 1 AND Exemplaire.disponibilite = TRUE;
+WHERE Exemplaire.code_ressource = 1 AND Exemplaire.disponibilite = TRUE;
 
 --"Modifier le synopsis d’un film ( = description d’une ressource) "
 UPDATE Film

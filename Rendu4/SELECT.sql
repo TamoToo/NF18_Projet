8555d5aeddb73c89dfbcccd2dc0684076c9dc904
Mathis Khaznadji <mathis.khaznadji@etu.utc.fr>
2022-11-17 10:22:10 +0000

    Update SELECT.sql

--- Rendu4/SELECT.sql
+++ Rendu4/SELECT.sql
@@ -8,4 +8,12 @@
 
 "Renvoie la liste des personnes ayant eu des sanctions triés par le nombre de jours de retard le plus élevé"
 
+" Classement des ressources empruntées de la plus empruntée à la moins empruntée "
+
+SELECT COUNT(*) AS Nombre_d_emprunt,R.titre
+FROM Ressource R
+INNER JOIN Exemplaire E ON E.ressource = R.code
+INNER JOIN Pret P ON P.code = E.code
+GROUP BY R.code 
+ORDER BY COUNT(*) DESC;
 

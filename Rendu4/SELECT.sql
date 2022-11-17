0ecc9527000a9f58a24539c8826b7a2b054988e4
Cyril Normand <cyril.normand@etu.utc.fr>
2022-11-17 09:52:08 +0000

    Update Rendu4/SELECT.sql

---
+++ Rendu4/SELECT.sql
@@ -0,0 +1,11 @@
+"Renvoie la liste des personnes ayant emprunter un livre entre le 11/02/2001 et le 11/02/2022"
+
+"Renvoie le nombre de livre empruntés par Tom Holland" 
+
+"Renvoie le nombre de personne ayant emprunté le film top gun"
+
+"Renvoie la liste des personnes ayant eu des sanctions"
+
+"Renvoie la liste des personnes ayant eu des sanctions triés par le nombre de jours de retard le plus élevé"
+
+

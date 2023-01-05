ee9fc6f449153a40bd00d60e129bd5f74e608ce3
Thomas <tamoto73@gmail.com>
2023-01-05 10:42:37 +0100

    Rendu6

---
+++ Rendu6/INSERT_V3.sql
@@ -0,0 +1,50 @@
+INSERT INTO Ressource VALUES (1, 'top gun', TO_DATE('1986-09-17', 'YYYY-MM-DD'), 'paramount pictures', 'action',  115 );
+INSERT INTO Ressource VALUES (2, 'harry potter', TO_DATE('1997-06-26', 'YYYY-MM-DD'), 'gallimard jeunesse', 'fantaisie',  299 );
+INSERT INTO Ressource VALUES (4, 'tchikita', TO_DATE('2016-09-19', 'YYYY-MM-DD'), 'jul', 'rap',  334 );
+
+
+INSERT INTO Livre VALUES (2, 34573, 'Harry Potter est un garçon ordinaire. Mais, le jour de ses onze ans, son existence bascule : un géant vient le chercher pour l emmener dans une école de sorciers. Voler à cheval sur des balais, jeter des sorts, combattre les Trolls: Harry Potter se révèle être un sorcier vraiment doué.', 'anglais');
+
+INSERT INTO Musique VALUES (4,'15:02:00');
+
+INSERT INTO Film VALUES (1,'Dans l océan Indien à la fin des années 1980, durant les dernières années de la guerre froide. Au cours d une patrouille aérienne de routine, deux chasseurs F-14 Tomcat de l US Navy embarqués sur le porte-avions USS Enterprise rencontrent deux chasseurs MiG-28 soviétiques, au comportement hostile. Les pilotes américains parviennent à mettre les avions soviétiques en fuite mais le leader de la formation, Bill « Cougar » Cortell, se retrouve en état de choc à la suite de l accrochage. Le pilote de l autre appareil américain, Pete « Maverick » Mitchell — un aviateur avec un tempérament de « chien fou », peu apprécié de sa hiérarchie et marqué par la disparition en vol de son père, le 5 novembre 1965, lors de la guerre du Viêt Nam — retourne alors chercher Cougar, malgré les ordres reçus et le manque de kérosène de son avion, et l aide à se poser sur le porte-avions.','01:40:00', 'français');
+
+
+INSERT INTO Contributeur VALUES (1, 'cruise', 'tom', TO_DATE('1862-07-03', 'YYYY-MM-DD'), 'americain');
+INSERT INTO Contributeur VALUES (4, 'mari', 'julien', TO_DATE('1990-01-14', 'YYYY-MM-DD'), 'francais');
+INSERT INTO Contributeur VALUES (2, ' Rowling', 'J.K.', TO_DATE('1965-07-31', 'YYYY-MM-DD'), 'britannique');
+
+INSERT INTO Exemplaire VALUES (1, TRUE, 'neuf');
+INSERT INTO Exemplaire VALUES (2, TRUE, 'bon');
+INSERT INTO Exemplaire VALUES (4, TRUE, 'perdu');
+
+
+
+
+
+INSERT INTO Interprete VALUES (4,4);
+INSERT INTO Compositeur VALUES (4,4);
+
+INSERT INTO Auteur VALUES (2, 2);
+
+INSERT INTO Acteur VALUES (1,1);
+
+INSERT INTO Realisateur VALUES (1,1);
+
+
+
+
+INSERT INTO Personne VALUES ('tom.cruise@etu.utc.fr', 'cruise', 'tom', 'rue de l avion');
+INSERT INTO Personne VALUES ('tom.holland@etu.utc.fr', 'holland', 'tom', 'avenue de l araignée');
+
+INSERT INTO Adherent VALUES ('tom.cruise@etu.utc.fr', TO_DATE('1862-07-03', 'YYYY-MM-DD'), 0669354585, 'zebi13', TRUE, TRUE);
+
+INSERT INTO Personnel VALUES ('tom.holland@etu.utc.fr', 'directeur');
+
+INSERT INTO Compte_Utilisateur VALUES ('cruisetom','tom.cruise@etu.utc.fr', 'tomi60200');
+INSERT INTO Compte_Utilisateur VALUES ('hollandtom', 'tom.holland@etu.utc.fr', 'tomi12345');
+
+
+INSERT INTO Pret VALUES (2, 'tom.cruise@etu.utc.fr', TO_DATE('2013-02-07', 'YYYY-MM-DD'), 3);
+INSERT INTO Retard VALUES (1, 4, 'tom.cruise@etu.utc.fr');
+INSERT INTO Deterioration VALUES (1, 'abimé', 0, 'tom.cruise@etu.utc.fr');
---
+++ Rendu6/SELECT_V3.sql
@@ -0,0 +1,110 @@
+/*"Faire deux requête avec les contraintes"
+
+"Genre soustraire la date de fin et la date de debut"*/
+
+
+--"Afficher les ressources disponibles"
+SELECT titre FROM Ressource 
+JOIN Exemplaire ON Exemplaire.code_ressource = Ressource.code 
+WHERE disponibilite = TRUE;
+
+--"Renvoie le nombre de livre empruntes par Tom Holland" 
+SELECT COUNT(*) FROM Ressource 
+JOIN Pret ON Ressource.code = Pret.code 
+JOIN Adherent ON Adherent.email = Pret.adherent
+JOIN Personne ON Personne.email = Adherent.email 
+WHERE Personne.nom = "holland" AND Personne.prenom = "tom";
+
+--"Renvoie le nombre de personne ayant emprunte le film top gun"
+SELECT COUNT(*) FROM Adherent 
+JOIN Pret ON Pret.adherent = Adherent.email
+JOIN Ressource ON Ressource.code = Pret.code
+WHERE Ressource.titre = "top gun"; 
+
+
+--"Renvoie la liste des personnes ayant perud l'article empreinte"
+SELECT nom , prenom FROM Personne 
+JOIN Adherent ON Adherent.email = Personne.email
+JOIN Deterioration ON deterioration.adherent = Adherent.email 
+WHERE Deterioration.nv_etat = "perdu";
+
+--"Renvoie la liste des personnes ayant eu des jours de retards, tries par le nombre de jours de retard le plus eleve au moins eleve"
+SELECT nom , prenom FROM Personne 
+JOIN Adherent ON Adherent.email = Personne.email 
+JOIN Retard ON Retard.adherent = Adherent.email
+WHERE nb_jours > 0
+ORDER BY nb_jours DESC;
+
+--"Renvoie le nombre de jours de retards maximal"
+SELECT max(nb_jours) FROM Retard;
+
+--"Renvoie la proportion des personnes suspendu"
+SELECT  COUNT(suspension)/COUNT(*) FROM Adherent WHERE suspension = TRUE;
+
+--"Revnoie la moyenne des jours de retards"
+SELECT AVG(nb_jours) FROM Retard;
+ 
+--"Classement des ressources empruntees de la plus empruntee à la moins empruntee "
+
+SELECT COUNT(*) AS Nombre_d_emprunt,R.titre
+FROM Ressource R
+INNER JOIN Exemplaire E ON E.code_ressource = R.code
+INNER JOIN Pret P ON P.code = E.code_ressource
+GROUP BY R.code 
+ORDER BY COUNT(*) DESC;
+
+--"En combien d'exemplaires disponibles existe une ressource donnee (ici la ressource 1) "
+
+SELECT COUNT(*) AS nb_exemplaires_dispo_ressource1
+FROM Exemplaire
+WHERE Exemplaire.code_ressource = 1 AND Exemplaire.disponibilite = TRUE;
+
+--"Modifier le synopsis d’un film ( = description d’une ressource) "
+UPDATE Film
+SET synopsis = 'ceci est un synopsis'
+WHERE Film.code = '3';
+
+--"Regarder le synopsis d'un film "
+SELECT synopsis 
+FROM Film 
+WHERE Film.code = '3';
+
+
+--"Classement des ressources empruntées de la plus empruntée à la moins empruntée pour une année donnée"
+SELECT COUNT(*) AS Nombre_d_emprunt_mois_de_mai,R.titre
+FROM Ressource R
+INNER JOIN Exemplaire E ON E.ressource = R.code
+INNER JOIN Pret P ON P.code = E.code
+WHERE P.date BETWEEN TO_DATE('20210601','YYYYMMDD') AND TO_DATE('20220601','YYYYMMDD')
+GROUP BY R.code 
+ORDER BY COUNT(*) DESC;
+ 
+
+--"Le nombre d'exemplaire pour chaque oeuvre"
+SELECT Count(Ressource.code), titre
+FROM Exemplaire
+JOIN Livre ON Exemplaire.ressource = Livres.code
+GROUP BY titre
+
+UNION
+
+SELECT Count(Ressource.code), titre
+FROM Exemplaire
+JOIN Film ON Exemplaire.ressource = Films.code
+GROUP BY titre
+
+UNION
+
+SELECT Count(Ressource.code), titre
+FROM Exemplaire
+JOIN Musique ON Exemplaire.ressource = Musiques.code
+GROUP BY titre;
+
+
+--"Le nombre de livre dans un état perdu"
+SELECT code FROM Exemplaire 
+JOIN Deterioration ON Deterioration.id = Ressource.code
+WHERE Deterioration.nv_etat = 'perdu'
+GROUP BY Exemplaire.titre 
+
+
